ee82969b172fe1ec17d4b6ebc5f1cc2538df07df
abdulahad-gh <abdulahadmailacc@gmail.com>
2022-05-24 17:43:41 +0600

    laod all reviews, and also dislay reverse way

--- src/pages/Home/Reviews.js
+++ src/pages/Home/Reviews.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Reviews = () => {
+    const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+
+        fetch('http://localhost:5000/reviews', {
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+            }
+        })
+            .then(res => res.json())
+            .then(data => setReviews(data));
+    }, [])
+
     return (
 
         <section className='mt-36 px-10'>
@@ -11,39 +24,20 @@ const Reviews = () => {
 
 
             <div className='grid grid-cols-1 md:grid-cols-3'>
-                <div>
-                    <div class="flex items-center mb-1">
-                        <svg class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                        <span>5 star</span>
+                {
+                    reviews.map(review => <div>
+                        <div class="flex items-center mb-1">
+                            <svg class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                            <span>{review?.rating}</span>
 
 
-                    </div>
-                    <p>Nazim Howleder</p>
+                        </div>
+                        <p>{review?.reviewOwner}</p>
 
-                    <p class="mb-2 font-light text-gray-500 dark:text-gray-400">Your part is Good. I'am very interesting </p>
-                </div>
-                <div>
-                    <div class="flex items-center mb-1">
-                        <svg class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                        <span>3 star</span>
+                        <p class="mb-2 font-light text-gray-500 dark:text-gray-400">{review?.reviewDesc}</p>
+                    </div>).reverse()
+                }
 
-
-                    </div>
-                    <p>Shaib</p>
-
-                    <p class="mb-2 font-light text-gray-500 dark:text-gray-400">Your part is Good. I'am very interesting </p>
-                </div>
-                <div>
-                    <div class="flex items-center mb-1">
-                        <svg class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                        <span>3 star</span>
-
-
-                    </div>
-                    <p>Shaib</p>
-
-                    <p class="mb-2 font-light text-gray-500 dark:text-gray-400">Your part is Good. I'am very interesting </p>
-                </div>
             </div>
 
         </section>
